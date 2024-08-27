e031360f5801f287947763098ab8764cfee0a522
vinuja c <122460596+83vinu@users.noreply.github.com>
2024-08-27 16:23:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,48 @@
+import Content from "./Content";
+import Footer from "./Footer";
+import Header from "./Header";
+import { useState } from "react";
+
+function App() {  
+
+  const [items, setItems] = useState([
+    {id:1,
+     checked:true,
+     item:"practice codeing"   
+    },
+    {id:2,
+        checked:false,
+        item:"seeing youtube video"   
+       },
+       {id:3,
+        checked:false,
+        item:"implementing react"   
+       }
+]);
+const handleCheck = (id) =>{
+    const listItems =items.map((item)=> item.id===id ?{...item,checked:!item.checked} :item)
+    setItems(listItems)
+    localStorage.setItem("todo_list",JSON.stringify(listItems))
+}
+const handleDelete=(id)=>{
+    const listItems=items.filter((item)=>item.id!==id)
+    setItems(listItems)
+    localStorage.setItem("todo_list",JSON.stringify(listItems))
+}
+
+  return (
+    <div className="App">
+       <Header title="To do List"/>
+       <Content 
+       items={items}
+       handleCheck={handleCheck}
+       handleDelete={handleDelete}
+       />
+       <Footer
+       length ={items.length}
+       />
+    </div>
+  );
+}
+
+export default App;
